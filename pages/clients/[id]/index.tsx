4039e4359d6fe29ede264865fd0d4bc1bc868d12
Jovan Danilovic <joco-95@hotmail.com>
2022-10-04 18:58:28 +0200

    feat: navigate programatically

--- pages/clients/[id]/index.tsx
+++ pages/clients/[id]/index.tsx
@@ -2,10 +2,14 @@ import { useRouter } from "next/router";
 
 export default function ClientProjectsPage() {
   const router = useRouter();
+  const loadProjectHandler = () => {
+    router.push("/clients/max/projectA");
+  };
   console.log(router.query);
   return (
     <div>
       <h1>The Projects of Given client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
