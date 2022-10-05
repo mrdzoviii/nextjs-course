d311d26c1457f30489d272886ea8bc6c94163bab
Jovan Danilovic <joco-95@hotmail.com>
2022-10-05 19:53:55 +0200

    feat: added filter event page

--- pages/events/[...slug].tsx
+++ pages/events/[...slug].tsx
@@ -1,13 +1,41 @@
 import { useRouter } from "next/router";
+import EventList from "../../components/events/event-list";
+import { getFilteredEvents } from "../../dummy-data";
 
 export default function FilteredEventsPage() {
   const router = useRouter();
-  const slug = router.query.slug;
-  console.log(slug);
+  const filterData = router.query.slug as string[];
+
+  if (!filterData) {
+    return <p className="center">Loading...</p>;
+  }
+
+  const numYear = +filterData[0];
+  const numMonth = +filterData[1];
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return <p>Invalid filter. Please adjust your values.</p>;
+  }
+
+  const events = getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  });
+
+  if (!events || events.length === 0) {
+    return <p>No events found for the choosen filter</p>;
+  }
 
   return (
-    <div>
-      <h1>Filtered Events Page</h1>
-    </div>
+    <>
+      <EventList events={events} />
+    </>
   );
 }
