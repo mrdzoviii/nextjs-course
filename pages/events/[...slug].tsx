04def654ffa6f7efdf4ba3e2b7ca11c269eb5cba
Jovan Danilovic <joco-95@hotmail.com>
2022-10-05 10:50:03 +0200

    feat: added main pages. dummy data util introduced

---
+++ pages/events/[...slug].tsx
@@ -0,0 +1,12 @@
+import { useRouter } from "next/router";
+
+export default function FilteredEventsPage() {
+  const router = useRouter();
+  console.log(router.query);
+
+  return (
+    <div>
+      <h1>Filtered Events Page</h1>
+    </div>
+  );
+}
