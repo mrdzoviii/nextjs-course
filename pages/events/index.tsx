f3014ba859ed209d830c003436be2d5bc3291334
Jovan Danilovic <joco-95@hotmail.com>
2022-10-05 18:41:55 +0200

    feat: added all events page

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -1,7 +1,11 @@
-export default function EventsPage() {
+import EventList from "../../components/events/event-list";
+import { getAllEvents } from "../../dummy-data";
+
+export default function AllEventsPage() {
+  const events = getAllEvents();
   return (
     <div>
-      <h1>The Events Page</h1>
+      <EventList events={events} />
     </div>
   );
 }
