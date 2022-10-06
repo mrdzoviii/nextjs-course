1ed78436e6d79303f0c5b010e1309b43de4871be
Jovan Danilovic <joco-95@hotmail.com>
2022-10-06 10:21:00 +0200

    feat: added data json

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
 export default function HomePage() {
   return (
     <div>
-      <h1>The Home Page</h1>
+      <ul>
+        <li>Product 1</li>
+        <li>Product 2</li>
+        <li>Product 3</li>
+      </ul>
     </div>
   );
 }
