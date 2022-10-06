4164fda83571f199ffb520354fd31dc8f044dcc4
Jovan Danilovic <joco-95@hotmail.com>
2022-10-06 10:33:57 +0200

    feat: added getStaticProps to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,32 @@
-export default function HomePage() {
+import { GetStaticProps, GetStaticPropsContext, NextPage } from "next";
+import { Product } from "../types";
+
+export interface IHomePageProps {
+  products: Product[];
+}
+
+const HomePage: NextPage<IHomePageProps> = ({ products }) => {
   return (
     <div>
       <ul>
-        <li>Product 1</li>
-        <li>Product 2</li>
-        <li>Product 3</li>
+        {products.map((product) => (
+          <li key={product.id}>{product.title}</li>
+        ))}
       </ul>
     </div>
   );
-}
+};
+
+//can do server side things
+//code won't be run on client-side and this is not part of client bundle
+export const getStaticProps: GetStaticProps<IHomePageProps> = async () => {
+  return {
+    props: {
+      products: [
+        { id: "p1", title: "Product 1", description: "Product 1 description" },
+      ],
+    },
+  };
+};
+
+export default HomePage;
