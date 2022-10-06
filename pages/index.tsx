e6fca8106803d5105ba6c828e52f24f882a89eae
Jovan Danilovic <joco-95@hotmail.com>
2022-10-06 10:10:14 +0200

    ci: empty next js project setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,7 @@
-import { NextPage } from "next";
-
-const HomePage: NextPage = () => {
+export default function HomePage() {
   return (
     <div>
       <h1>The Home Page</h1>
     </div>
   );
-};
-
-export default HomePage;
+}
