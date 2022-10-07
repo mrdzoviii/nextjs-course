73dec93210066d3aa152d73e1637de5abf73d35f
Jovan Danilovic <joco-95@hotmail.com>
2022-10-07 18:57:37 +0200

    feat: added ssg on home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,29 @@
+import { GetStaticProps, NextPage } from "next";
 import EventList from "../components/events/event-list";
-import { getFeaturedEvents } from "../dummy-data";
+import { Event, getFeaturedEvents } from "../dummy-data";
 
-export default function HomePage() {
-  const featuredEvents = getFeaturedEvents();
+export interface IHomePageProps {
+  featuredEvents: Event[];
+}
+
+const HomePage: NextPage<IHomePageProps> = ({ featuredEvents }) => {
   return (
     <div>
       <EventList events={featuredEvents} />
     </div>
   );
-}
+};
+
+export const getStaticProps: GetStaticProps<IHomePageProps> = async () => {
+  const response = await fetch(
+    "https://next-js-database-default-rtdb.europe-west1.firebasedatabase.app/events.json?print=pretty&orderBy=%22isFeatured%22&equalTo=true"
+  );
+  const data = await response.json();
+  const events = [];
+  for (let key in data) {
+    events.push({ id: key, ...data[key] });
+  }
+  return { props: { featuredEvents: events }, revalidate: 60 * 60 * 24 };
+};
+
+export default HomePage;
