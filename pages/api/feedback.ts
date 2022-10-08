ddb81e3ebfc7b36969c2df45a8cf90bdb900830d
Jovan Danilovic <joco-95@hotmail.com>
2022-10-08 19:46:17 +0200

    feat: sending request to api routes

--- pages/api/feedback.ts
+++ pages/api/feedback.ts
@@ -1,7 +1,26 @@
+import fs from "fs";
+import path from "path";
 import { NextApiHandler } from "next";
 
 const handler: NextApiHandler = (req, res) => {
-  res.status(200).json({ message: "This works!" });
+  if (req.method === "POST") {
+    console.log(req.body);
+    const { email, feedback } = req.body;
+    const newFeedback = {
+      id: Date.now(),
+      email,
+      feedback,
+    };
+
+    const filePath = path.join(process.cwd(), "data", "feedback.json");
+    const fileData = JSON.parse(fs.readFileSync(filePath).toString());
+    fileData.push(newFeedback);
+    fs.writeFileSync(filePath, JSON.stringify(fileData));
+
+    res.status(201).json({ message: "Success", feedback: newFeedback });
+  } else {
+    res.status(400).json({ message: "Wrong method" });
+  }
 };
 
 export default handler;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,39 @@
 import type { NextPage } from "next";
+import { EventHandler, FormEvent, FormEventHandler, useRef } from "react";
 
 const Home: NextPage = () => {
+  const emailRef = useRef<HTMLInputElement>();
+  const feedbackRef = useRef<HTMLTextAreaElement>();
+
+  const submitFormHandler = async (event: FormEvent) => {
+    event.preventDefault();
+    const emailValue = emailRef.current.value;
+    const feedbackValue = feedbackRef.current.value;
+    const response = await fetch(`/api/feedback`, {
+      method: "POST",
+      body: JSON.stringify({ email: emailValue, feedback: feedbackValue }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const message = await response.json();
+    console.log(message);
+  };
+
   return (
     <div>
       <h1>The Home Page</h1>
+      <form onSubmit={submitFormHandler}>
+        <div>
+          <label htmlFor="email">Your Email Address</label>
+          <input ref={emailRef} type="email" id="email" />
+        </div>
+        <div>
+          <label htmlFor="feedback">Your Feedback</label>
+          <textarea ref={feedbackRef} rows={5} id="feedback" />
+        </div>
+        <button>Send feedback</button>
+      </form>
     </div>
   );
 };
