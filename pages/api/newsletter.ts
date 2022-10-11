8bdc6d98e1fd124da6b2e46ce90488bda1e049f6
Jovan Danilovic <joco-95@hotmail.com>
2022-10-11 16:55:19 +0200

    feat: added error handling

--- pages/api/newsletter.ts
+++ pages/api/newsletter.ts
@@ -1,6 +1,21 @@
 import { NextApiHandler } from "next";
 
-import { MongoClient } from "mongodb";
+import { MongoClient, Document } from "mongodb";
+
+const connectDB = async (): Promise<MongoClient> => {
+  const host = process.env.DB_HOST;
+  const username = process.env.DB_USER;
+  const pass = process.env.DB_PASS;
+  const client = await MongoClient.connect(
+    `mongodb+srv://${username}:${pass}@${host}/?retryWrites=true&w=majority`
+  );
+  return client;
+};
+
+const insertDocument = async (client: MongoClient, document: Document) => {
+  const db = client.db("events");
+  await db.collection("emails").insertOne(document);
+};
 
 const handler: NextApiHandler = async (req, res) => {
   if (req.method == "POST") {
@@ -11,18 +26,22 @@ const handler: NextApiHandler = async (req, res) => {
       return;
     }
 
-    const host = process.env.DB_HOST;
-    const username = process.env.DB_USER;
-    const pass = process.env.DB_PASS;
+    let client;
 
-    const client = await MongoClient.connect(
-      `mongodb+srv://${username}:${pass}@${host}/?retryWrites=true&w=majority`
-    );
+    try {
+      client = await connectDB();
+    } catch (err) {
+      res.status(500).json({ message: "Connecting to the db failed" });
+      return;
+    }
 
-    const db = client.db("events");
-    await db.collection("emails").insertOne({ email: userEmail });
-
-    client.close();
+    try {
+      await insertDocument(client, { email: userEmail });
+      client.close();
+    } catch (err) {
+      res.status(500).json({ message: "Inserting data failed" });
+      return;
+    }
 
     res.status(201).json({ message: "Signed up" });
   }
