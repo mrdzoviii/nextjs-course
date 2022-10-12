4928a7f38c20ff13a769db82753771f3ad98ef1b
Jovan Danilovic <joco-95@hotmail.com>
2022-10-12 11:08:28 +0200

    feat: added main pages

---
+++ pages/posts/[slug].tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const PostDetailPage: NextPage = () => {
+  return (
+    <div>
+      <h1>The Home Page</h1>
+    </div>
+  );
+};
+
+export default PostDetailPage;
---
+++ pages/posts/index.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const AllPostsPage: NextPage = () => {
+  return (
+    <div>
+      <h1>The Home Page</h1>
+    </div>
+  );
+};
+
+export default AllPostsPage;
