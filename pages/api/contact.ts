cf0f40746de864b8cbbc61833563889a39483e7d
Jovan Danilovic <joco-95@hotmail.com>
2022-10-12 16:12:03 +0200

    feat: storing using mongoDB

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -1,8 +1,12 @@
 import { NextApiHandler } from "next";
 
-import { IContactFormRequest } from "../../type-definitions";
+import { MongoClient } from "mongodb";
 
-const handler: NextApiHandler = (req, res) => {
+import { ContactForm, IContactFormRequest } from "../../type-definitions";
+
+const dbConnectionURL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/my-site?retryWrites=true&w=majority`;
+
+const handler: NextApiHandler = async (req, res) => {
   if (req.method === "POST") {
     const { email, name, message } = req.body as IContactFormRequest;
 
@@ -17,10 +21,30 @@ const handler: NextApiHandler = (req, res) => {
       res.status(422).json({ message: "Invalid input" });
       return;
     }
+    let client: MongoClient;
+    try {
+      client = await MongoClient.connect(dbConnectionURL);
+    } catch (err) {
+      res.status(500).json({ message: "Could not connect to db" });
+      return;
+    }
+    const newMessage = { email, name, message } as ContactForm;
+    try {
+      const db = client.db();
+      const result = await db.collection("messages").insertOne({
+        email: newMessage.email,
+        name: newMessage.name,
+        message: newMessage.message,
+      });
+      newMessage._id = result.insertedId.toString();
+    } catch (err) {
+      client.close();
+      res.status(500).json({ message: "Storing message failed" });
+      return;
+    }
 
-    //STORE IN DB
+    client.close();
 
-    const newMessage: IContactFormRequest = { email, name, message };
     res.status(201).json({
       message: "Successfully stored message",
       storedMessage: newMessage,
