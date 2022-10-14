7123cfc4ec85e6e8457f24bba31e55beb088a8f3
Jovan Danilovic <joco-95@hotmail.com>
2022-10-14 16:49:17 +0200

    feat: added unique email address validation

--- pages/api/auth/sign-up.ts
+++ pages/api/auth/sign-up.ts
@@ -5,29 +5,39 @@ import { hashPassword } from "../../../helpers/auth";
 import { connectToDatabase } from "./../../../helpers/db";
 
 const handler: NextApiHandler = async (req, res) => {
-  const { email, password } = req.body as LoginRequest;
+  if (req.method === "POST") {
+    const { email, password } = req.body as LoginRequest;
 
-  if (
-    !email ||
-    !email.includes("@") ||
-    !password ||
-    password.trim().length < 7
-  ) {
-    res.status(422).json({ message: "Email or password is invalid" });
-    return;
+    if (
+      !email ||
+      !email.includes("@") ||
+      !password ||
+      password.trim().length < 7
+    ) {
+      res.status(422).json({ message: "Email or password is invalid" });
+      return;
+    }
+
+    const client = await connectToDatabase();
+    const db = client.db();
+
+    const existingUser = db.collection("users").findOne({ email });
+    if (existingUser) {
+      res.status(409).json({ message: "User with given email already exists" });
+      client.close();
+      return;
+    }
+
+    const hashedPassword = await hashPassword(password);
+
+    const result = await db.collection("users").insertOne({
+      email: email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({ message: "Created user!" });
+    client.close();
   }
-
-  const client = await connectToDatabase();
-  const db = client.db();
-
-  const hashedPassword = await hashPassword(password);
-
-  const result = await db.collection("users").insertOne({
-    email: email,
-    password: hashedPassword,
-  });
-
-  res.status(201).json({ message: "Created user!" });
 };
 
 export default handler;
