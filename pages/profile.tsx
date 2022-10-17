4b80dde75f7d2fce3c9a4c78eb23ac0974c1a6a0
Jovan Danilovic <joco-95@hotmail.com>
2022-10-17 09:41:45 +0200

    feat: added server side guard

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,9 +1,29 @@
-import { NextPage } from "next";
+import { GetServerSideProps, NextPage } from "next";
+
+import { Session } from "next-auth";
+import { getSession } from "next-auth/react";
 
 import UserProfile from "../components/profile/user-profile";
 
-const ProfilePage: NextPage = () => {
+export interface IProfilePageProps {
+  session: Session;
+}
+
+const ProfilePage: NextPage<IProfilePageProps> = ({ session }) => {
   return <UserProfile />;
+};
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const session = await getSession({ req: ctx.req });
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+  return { props: { session } };
 };
 
 export default ProfilePage;
